476a230b207f5694d391420bc21706adeb3b2a88
Olha Mushket <olhamushket@gmail.com>
2022-07-04 19:34:11 +0300

    Solved

--- src/main/java/com/epam/rd/autotasks/TaskCarousel.java
+++ src/main/java/com/epam/rd/autotasks/TaskCarousel.java
@@ -1,25 +1,54 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
+
 public class TaskCarousel {
-
+    private final ArrayList<Task> tasks = new ArrayList<>();
+    private int counter = 0;
+    private final int COUNTER_MAX;
     public TaskCarousel(int capacity) {
-        throw new UnsupportedOperationException();
-    }
+        if (capacity <= 0) {
+            throw new UnsupportedOperationException();
+        }
+        COUNTER_MAX = capacity;
 
+    }
     public boolean addTask(Task task) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+   if (!isFull()&&!task.isFinished()) {
+       tasks.add(task);
+       return true;
+   }
+   return false;
     }
 
     public boolean execute() {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        if (isEmpty()) {                        //Carousel is Empty
+            return false;
+        }
+        isEnd();
+        tasks.get(counter).execute();
+        if (tasks.get(counter).isFinished()) {      //deleting of finished task
+            tasks.remove(counter);
+        } else {
+            counter++;
+        }
+        return true;
     }
 
     public boolean isFull() {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        return tasks.size() == COUNTER_MAX;
     }
 
     public boolean isEmpty() {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        return tasks.size() == 0;
     }
-
+    private void isEnd() {
+        if (counter >= tasks.size()) {      //execution by circle
+            counter = 0;
+        }
+    }
 }
